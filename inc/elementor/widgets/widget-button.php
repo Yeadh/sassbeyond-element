6ebe80c6dc0a7335b932265740814096982bf662
Farhan Khan <farhan01617@gmail.com>
2019-07-10 12:21:35 +0600

    Updarw

--- inc/elementor/widgets/widget-button.php
+++ inc/elementor/widgets/widget-button.php
@@ -81,7 +81,7 @@ class sassbeyond_Widget_Button extends Widget_Base {
       ?>
 
       <div class="sassbeyond-btn <?php if( $settings['color'] == 'yes' ){ echo 'alt-color';} ?>" style="text-align: <?php echo esc_attr($settings['align']) ?>">
-         <a href="<?php echo esc_url( $settings['button_url'] ); ?>">
+         <a class="btn" href="<?php echo esc_url( $settings['button_url'] ); ?>">
             <?php echo esc_html( $settings['button_text'] ); ?></a>
       </div>
       <?php
